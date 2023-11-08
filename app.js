d16c97f8d321e5429c3d4cb35f19964cf9561530
Jen Thomas James <jenthomasjames@gmail.com>
2023-11-08 21:22:12 +0530

    Dep: Add Cors package

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
-const app = express();
+const app = express(cors());
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("App running on port " + process.env.PORT || 3001);
